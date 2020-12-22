f6ae972773a2c9720baca623e0d38b0f03789977
Oleh <OlehKokhan334@gmail.com>
2020-12-22 20:20:36 +0200

    added Validator

---
+++ src/Task3/Validator.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <string>
+namespace Validator {
+	bool TryParseToUShort(const std::string data, unsigned short& outNum)
+	{
+		try {
+			size_t pos;
+			unsigned long tmp = std::stoul(data, &pos);
+			if (data.size() != pos)
+			{
+				throw false;
+			}
+			outNum = (unsigned short)(tmp <= USHRT_MAX ? tmp : throw false);
+		}
+		catch (...)
+		{
+			outNum = 0;
+			return false;
+		}
+
+		return true;
+	}
+
+	bool TryParseToFloat(const std::string data, float& outNum)
+	{
+		try
+		{
+			size_t pos;
+			outNum = stof(data, &pos);
+			if (data.size() != pos)
+			{
+				throw false;
+			}
+		}
+		catch (...)
+		{
+			outNum = 0;
+			return false;
+		}
+
+		return true;
+	}
+
+	bool TryParseToLongLong(const std::string data, long long& outNum)
+	{
+		try
+		{
+			size_t pos;
+			outNum = stoll(data, &pos);
+			if (data.size() != pos)
+			{
+				throw false;
+			}
+		}
+		catch (...)
+		{
+			outNum = 0;
+		
+			return false;
+		}
+
+		return true;
+	}
+
+	bool TryParseToDouble(const std::string data, double& outNum)
+	{
+		try
+		{
+			size_t pos;
+			outNum = stod(data, &pos);
+			if (data.size() != pos)
+			{
+				throw false;
+			}
+		}
+		catch (...)
+		{
+			outNum = 0;
+			return false;
+		}
+
+		return true;
+	}
+};
